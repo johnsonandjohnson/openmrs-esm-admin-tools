b9ef4c1c82644ebab071515cf9faf7cb75bc834c
druchniewicz <druchniewicz@soldevelo.com>
2024-05-17 14:13:38 +0200

    Changed title in delete schedule dialog

--- packages/esm-reports-app/src/components/run-report/cancel-report-modal.component.tsx
+++ packages/esm-reports-app/src/components/run-report/cancel-report-modal.component.tsx
@@ -1,9 +1,15 @@
-import React, { useCallback, useState } from 'react';
-import { ModalBody, Button, ModalFooter, ModalHeader, InlineLoading } from '@carbon/react';
-import { useTranslation } from 'react-i18next';
-import { cancelReportRequest } from '../reports.resource';
-import { showToast } from '@openmrs/esm-framework';
-import { mutate } from 'swr';
+import React, { useCallback, useState } from "react";
+import {
+  ModalBody,
+  Button,
+  ModalFooter,
+  ModalHeader,
+  InlineLoading,
+} from "@carbon/react";
+import { useTranslation } from "react-i18next";
+import { cancelReportRequest } from "../reports.resource";
+import { showToast } from "@openmrs/esm-framework";
+import { mutate } from "swr";
 
 interface CancelReportModalProps {
   closeModal: () => void;
@@ -11,7 +17,11 @@ interface CancelReportModalProps {
   modalType: string;
 }
 
-const CancelReportModal: React.FC<CancelReportModalProps> = ({ closeModal, reportRequestUuid, modalType }) => {
+const CancelReportModal: React.FC<CancelReportModalProps> = ({
+  closeModal,
+  reportRequestUuid,
+  modalType,
+}) => {
   const { t } = useTranslation();
   const [isCanceling, setIsCanceling] = useState(false);
 
@@ -23,7 +33,7 @@ const CancelReportModal: React.FC<CancelReportModalProps> = ({ closeModal, repor
         closeModal();
         showToast({
           critical: true,
-          kind: 'success',
+          kind: "success",
           title: getModalTitleByType(modalType),
           description: getSuccessToastMessageByType(modalType),
         });
@@ -31,7 +41,7 @@ const CancelReportModal: React.FC<CancelReportModalProps> = ({ closeModal, repor
       .catch((error) => {
         showToast({
           critical: true,
-          kind: 'error',
+          kind: "error",
           title: getModalTitleByType(modalType),
           description: getFailedToastMessageByType(modalType),
         });
@@ -39,80 +49,100 @@ const CancelReportModal: React.FC<CancelReportModalProps> = ({ closeModal, repor
   }, [closeModal]);
 
   const callMutates = (modalType) => {
-    let baseUrl = '/ws/rest/v1/reportingrest/reportRequest?statusesGroup=';
-    if (modalType === 'delete') {
-      mutate(baseUrl + 'ran');
-    } else if (modalType === 'cancel') {
-      mutate(baseUrl + 'ran');
-      mutate(baseUrl + 'processing');
-    } else if (modalType === 'schedule') {
-      mutate(baseUrl + 'scheduled&sortBy=name');
+    let baseUrl = "/ws/rest/v1/reportingrest/reportRequest?statusesGroup=";
+    if (modalType === "delete") {
+      mutate(baseUrl + "ran");
+    } else if (modalType === "cancel") {
+      mutate(baseUrl + "ran");
+      mutate(baseUrl + "processing");
+    } else if (modalType === "schedule") {
+      mutate(baseUrl + "scheduled&sortBy=name");
     }
   };
 
   const getModalTitleByType = (modalType) => {
-    if (modalType === 'delete') {
-      return t('deleteReport', 'Delete report');
-    } else if (modalType === 'cancel') {
-      return t('cancelReport', 'Cancel report');
-    } else if (modalType === 'schedule') {
-      return t('scheduleReport', 'Schedule report');
+    if (modalType === "delete") {
+      return t("deleteReport", "Delete report");
+    } else if (modalType === "cancel") {
+      return t("cancelReport", "Cancel report");
+    } else if (modalType === "schedule") {
+      return t("deleteSchedule", "Delete Schedule");
     }
   };
 
   const getModalBodyByType = (modalType) => {
-    if (modalType === 'delete') {
-      return t('deleteReportModalText', 'Are you sure you want to delete this report?');
-    } else if (modalType === 'cancel') {
-      return t('cancelReportModalText', 'Are you sure you want to cancel this report?');
-    } else if (modalType === 'schedule') {
-      return t('deleteReportScheduleModalText', 'Are you sure you want to delete this schedule?');
+    if (modalType === "delete") {
+      return t(
+        "deleteReportModalText",
+        "Are you sure you want to delete this report?"
+      );
+    } else if (modalType === "cancel") {
+      return t(
+        "cancelReportModalText",
+        "Are you sure you want to cancel this report?"
+      );
+    } else if (modalType === "schedule") {
+      return t(
+        "deleteReportScheduleModalText",
+        "Are you sure you want to delete this schedule?"
+      );
     }
   };
 
   const getSuccessToastMessageByType = (modalType) => {
-    if (modalType === 'delete') {
-      return t('reportDeletedSuccessfully', 'Report deleted successfully');
-    } else if (modalType === 'cancel') {
-      return t('reportCancelledSuccessfully', 'Report cancelled successfully');
-    } else if (modalType === 'schedule') {
-      return t('reportScheduleDeletedSuccessfully', 'Report schedule deleted successfully');
+    if (modalType === "delete") {
+      return t("reportDeletedSuccessfully", "Report deleted successfully");
+    } else if (modalType === "cancel") {
+      return t("reportCancelledSuccessfully", "Report cancelled successfully");
+    } else if (modalType === "schedule") {
+      return t(
+        "reportScheduleDeletedSuccessfully",
+        "Report schedule deleted successfully"
+      );
     }
   };
 
   const getFailedToastMessageByType = (modalType) => {
-    if (modalType === 'delete') {
-      return t('reportDeletingErrorMsg', 'Error during report deleting');
-    } else if (modalType === 'cancel') {
-      return t('reportCancelingErrorMsg', 'Error during report canceling');
-    } else if (modalType === 'schedule') {
-      return t('reportScheduleDeletingErrorMsg', 'Error during report schedule deleting');
+    if (modalType === "delete") {
+      return t("reportDeletingErrorMsg", "Error during report deleting");
+    } else if (modalType === "cancel") {
+      return t("reportCancelingErrorMsg", "Error during report canceling");
+    } else if (modalType === "schedule") {
+      return t(
+        "reportScheduleDeletingErrorMsg",
+        "Error during report schedule deleting"
+      );
     }
   };
 
   const getLoadingMessageByType = (modalType) => {
-    if (modalType === 'delete' || modalType === 'schedule') {
-      return t('deleting', 'Deleting');
-    } else if (modalType === 'cancel') {
-      return t('canceling', 'Canceling');
+    if (modalType === "delete" || modalType === "schedule") {
+      return t("deleting", "Deleting");
+    } else if (modalType === "cancel") {
+      return t("canceling", "Canceling");
     }
   };
 
   return (
     <div>
-      <ModalHeader closeModal={closeModal} title={getModalTitleByType(modalType)} />
+      <ModalHeader
+        closeModal={closeModal}
+        title={getModalTitleByType(modalType)}
+      />
       <ModalBody>
         <p>{getModalBodyByType(modalType)}</p>
       </ModalBody>
       <ModalFooter>
         <Button kind="secondary" onClick={closeModal}>
-          {t('no', 'No')}
+          {t("no", "No")}
         </Button>
         <Button kind="danger" onClick={handleCancel} disabled={isCanceling}>
           {isCanceling ? (
-            <InlineLoading description={getLoadingMessageByType(modalType) + '...'} />
+            <InlineLoading
+              description={getLoadingMessageByType(modalType) + "..."}
+            />
           ) : (
-            <span>{t('yes', 'Yes')}</span>
+            <span>{t("yes", "Yes")}</span>
           )}
         </Button>
       </ModalFooter>
